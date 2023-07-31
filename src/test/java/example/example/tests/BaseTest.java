2725cf8c2f86fa9dc450815eddaba53004dd5240
Shashi Mate(UST <208011@ust-global.com>
2023-07-31 19:13:45 +0530

    updated chrome driver path

--- src/test/java/example/example/tests/BaseTest.java
+++ src/test/java/example/example/tests/BaseTest.java
@@ -65,7 +65,7 @@ public class BaseTest {
 	 */
 	@BeforeClass
 	protected void setup() {
-		String chromeDriverPath = "/usr/bin/";
+		String chromeDriverPath = "/usr/bin/chromedriver";
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 //		WebDriverManager.chromedriver().setup();
 //		ChromeOptions ops = new ChromeOptions();
